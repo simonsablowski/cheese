75fbb83c4a74d9d7febd6f37bde557f650bd2716
Simon Sablowski <contact@simsab.net>
2011-04-07 08:50:57 +0000

    trivial layout change

--- views/Model.form.php
+++ views/Model.form.php
@@ -1,6 +1,6 @@
 <? $this->displayView('components/header.php'); ?>
 			<h1>
-				<a href="<? echo $ObjectName; ?>/index" title="<? echo $ObjectName; ?>"><? echo $this->localize($ObjectName); ?></a> &raquo; <? echo $this->localize(ucfirst($mode)); ?>
+				<a href="<? echo $ObjectName; ?>/index" title="<? echo $ObjectName; ?>"><? echo $this->localize($ObjectName); ?></a>&ensp;&raquo;&ensp;<? echo $this->localize(ucfirst($mode)); ?>
 
 			</h1>
 			<form action="<? echo sprintf('%s/%s', $ObjectName, $mode != 'update' ? $mode : sprintf('%s/%s', $mode, implode('/', $Object->getPrimaryKeyValue()))); ?>" method="post">
