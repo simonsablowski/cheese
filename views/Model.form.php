c710f0366f8b6a3ec1b8bd1e6ff4bef494cf9ca9
Simon Sablowski <contact@simsab.net>
2011-07-10 01:16:02 +0000

    corrected base path for links

--- views/Model.form.php
+++ views/Model.form.php
@@ -1,6 +1,6 @@
 <? $this->displayView('components/header.php'); ?>
 			<h1>
-				<a href="<? echo $this->getApplication()->getConfiguration('cheeseUrl') . $ObjectName; ?>/index" title="<? echo $ObjectName; ?>"><? echo $this->localize($ObjectName); ?></a>&ensp;&raquo;&ensp;<? echo $this->localize(ucfirst($mode)); ?>
+				<a href="<? echo $this->getApplication()->getConfiguration('baseUrl') . $ObjectName; ?>/index" title="<? echo $ObjectName; ?>"><? echo $this->localize($ObjectName); ?></a>&ensp;&raquo;&ensp;<? echo $this->localize(ucfirst($mode)); ?>
 
 			</h1>
 			<form action="<? echo sprintf('%s/%s', $ObjectName, $mode != 'update' ? $mode : sprintf('%s/%s', $mode, implode('/', $Object->getPrimaryKeyValue()))); ?>" method="post">
--- views/Model.index.php
+++ views/Model.index.php
@@ -1,9 +1,9 @@
 <? $this->displayView('components/header.php'); ?>
 			<h1>
-				<a href="<? echo $this->getApplication()->getConfiguration('cheeseUrl') . $ObjectName; ?>/index" title="<? echo $this->localize($ObjectName); ?>"><? echo $this->localize($ObjectName); ?></a>
+				<a href="<? echo $this->getApplication()->getConfiguration('baseUrl') . $ObjectName; ?>/index" title="<? echo $this->localize($ObjectName); ?>"><? echo $this->localize($ObjectName); ?></a>
 			</h1>
 			<div class="options">
-				<a class="option" href="<? echo $this->getApplication()->getConfiguration('cheeseUrl') . $ObjectName; ?>/create" title="<? echo $this->localize('Create'); ?>"><? echo $this->localize('Create'); ?></a>
+				<a class="option" href="<? echo $this->getApplication()->getConfiguration('baseUrl') . $ObjectName; ?>/create" title="<? echo $this->localize('Create'); ?>"><? echo $this->localize('Create'); ?></a>
 			</div>
 			<table class="content">
 				<thead class="head">
@@ -50,10 +50,10 @@
 						</td>
 <? endforeach; ?>
 						<td class="<? if ($n + 1 == count($Objects)): ?>last <? endif; ?>option">
-							<a href="<? echo $this->getApplication()->getConfiguration('cheeseUrl') . $ObjectName; ?>/update/<? echo implode('/', $Object->getPrimaryKeyValue()); ?>" title="<? echo $this->localize('Update'); ?>"><? echo $this->localize('Update'); ?></a>
+							<a href="<? echo $this->getApplication()->getConfiguration('baseUrl') . $ObjectName; ?>/update/<? echo implode('/', $Object->getPrimaryKeyValue()); ?>" title="<? echo $this->localize('Update'); ?>"><? echo $this->localize('Update'); ?></a>
 						</td>
 						<td class="<? if ($n + 1 == count($Objects)): ?>last <? endif; ?>option">
-							<a href="<? echo $this->getApplication()->getConfiguration('cheeseUrl') . $ObjectName; ?>/delete/<? echo implode('/', $Object->getPrimaryKeyValue()); ?>" title="<? echo $this->localize('Delete'); ?>"><? echo $this->localize('Delete'); ?></a>
+							<a href="<? echo $this->getApplication()->getConfiguration('baseUrl') . $ObjectName; ?>/delete/<? echo implode('/', $Object->getPrimaryKeyValue()); ?>" title="<? echo $this->localize('Delete'); ?>"><? echo $this->localize('Delete'); ?></a>
 						</td>
 					</tr>
 <? endforeach; ?>
