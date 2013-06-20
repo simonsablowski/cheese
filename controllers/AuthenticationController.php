bb276a036419f42145a59d53dff47b6cd708aaf0
Simon Sablowski <contact@simsab.net>
2013-06-20 01:02:29 +0000

    adding sign in and sign out functionality that support form views

--- controllers/AuthenticationController.php
+++ controllers/AuthenticationController.php
@@ -1,44 +1,71 @@
 <?php
 
-abstract class AuthenticationController extends Controller {
+class AuthenticationController extends Controller {
 	protected $User;
 	
-	protected function signIn($userName, $password) {
-		try {
-			$this->setUser(User::findByUserNameAndPassword(array(
-				$userName,
-				sha1($password . $this->getConfiguration('encryptionKey'))
-			)));
-			return $this->getSession()->setData('User', $this->getUser());
-		} catch(Error $Error) {
-			return FALSE;
-		}
+	public function getFields() {
+		$Fields = array();
+		
+		$Fields[] = new TextField('userName', 'User Name', 100);
+		
+		$Fields[] = new PasswordField('password', 'Password', 40);
+		
+		return $Fields;
 	}
 	
-	protected function authenticate() {
-		if (!is_null($this->getSession()->getData('User'))) {
-			return TRUE;
+	public function signIn() {
+		if ($this->authenticate()) {
+			throw new FatalError('Authenticated', $this->getSession()->getData('User')->getUserName());
 		}
 		
-		$userName = $this->getRequest()->getData('PHP_AUTH_USER');
-		$password = $this->getRequest()->getData('PHP_AUTH_PW');
+		if ($this->getConfiguration('basicAuthentication')) {
+			$userName = $this->getRequest()->getData('PHP_AUTH_USER');
+			$password = $this->getRequest()->getData('PHP_AUTH_PW');
+		} else {
+			$userName = $this->getRequest()->getData('userName');
+			$password = $this->getRequest()->getData('password');
+		}
 		
 		if ($userName && $password) {
-			if ($this->signIn($userName, $password)) {
-				return TRUE;
-			} else {
+			try {
+				$this->setUser(User::findByUserNameAndPassword(array(
+					$userName,
+					sha1($password . $this->getConfiguration('encryptionKey'))
+				)));
+				$this->getSession()->setData('User', $this->getUser());
+				return $this->redirect();
+			} catch(Error $Error) {
 				throw new FatalError('Unauthorized', $userName);
 			}
 		} else {
-			$realmName = $this->localize($this->getConfiguration('pageTitle'));
-			header(sprintf('WWW-Authenticate: Basic realm="%s"', $realmName));
-			header('HTTP/1.0 401 Authorization Required');
+			if ($this->getConfiguration('basicAuthentication')) {
+				$realmName = $this->localize($this->getConfiguration('pageTitle'));
+				header(sprintf('WWW-Authenticate: Basic realm="%s"', $realmName));
+				header('HTTP/1.0 401 Authorization Required');
+			} else {
+				return $this->displayView('Authentication.signIn.php', array(
+					'Fields' => self::getFields(),
+					'query' => $this->getRequest()->getConfiguration('defaultQuery')
+				));
+			}
 		}
+	}
+	
+	public function signOut() {
+		$this->setUser(NULL);
+		$this->getSession()->setData('User', $this->getUser());
+		return $this->redirect();
+	}
+	
+	protected function authenticate() {
+		return $this->getSession()->getData('User') instanceof User;
 	}
 	
 	protected function performAction($actionName, $parameters) {
 		if ($this->authenticate()) {
 			return parent::performAction($actionName, $parameters);
+		} else {
+			return $this->signIn();
 		}
 	}
 }
---
+++ views/Authentication.signIn.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+		<meta http-equiv="Content-Language" content="en"/>
+		<title><? echo $this->localize('Authentication'); ?></title>
+		<link href="<? echo $this->getApplication()->getConfiguration('cheeseUrl'); ?>css/style.css" rel="stylesheet" title="Default" type="text/css" />
+	</head>
+	<body>
+		<div id="document">
+			<h1>
+				<? echo $this->localize('Authentication'); ?>
+
+			</h1>
+			<form action="<? echo $this->link($query); ?>" method="post">
+				<fieldset>
+					<table class="content">
+						<tbody class="body">
+<? foreach ($Fields as $n => $Field): ?>
+							<tr class="<? echo $n % 2 ? 'odd' : 'even'; ?>">
+								<td class="field">
+									<? echo $this->localize($Field->getLabel()); ?>
+
+								</td>
+								<td>
+<? if ($Field instanceof PasswordField): ?>
+									<input type="password" name="<? echo $Field->getName(); ?>" value=""/>
+<? else: ?>
+									<input type="text" name="<? echo $Field->getName(); ?>" value="<? echo ($value = $this->getRequest()->getData($Field->getName())) ? $value : ''; ?>"/>
+<? endif; ?>
+								</td>
+							</tr>
+<? endforeach; ?>
+						</tbody>
+						<tfoot class="foot">
+							<tr class="last">
+								<td>
+									&nbsp;
+								</td>
+								<td>
+									<input class="submit" type="submit" name="submit" value="<? echo $this->localize('Submit'); ?>"/>
+								</td>
+							</tr>
+						</tfoot>
+					</table>
+				</fieldset>
+			</form>
+		</div>
+	</body>
+</html>
