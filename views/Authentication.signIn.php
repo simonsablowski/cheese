13506bff9f2d5e462c2e2f8c700b908bd575d8cd
Simon Sablowski <contact@simsab.net>
2013-06-20 09:24:40 +0000

    including header and footer in signIn view

--- views/Authentication.signIn.php
+++ views/Authentication.signIn.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-		<meta http-equiv="Content-Language" content="en"/>
-		<title><? echo $this->localize('Authentication'); ?></title>
-		<link href="<? echo $this->getApplication()->getConfiguration('cheeseUrl'); ?>css/style.css" rel="stylesheet" title="Default" type="text/css" />
-	</head>
-	<body>
-		<div id="document">
+<? $this->displayView('components/header.php'); ?>
 			<h1>
 				<? echo $this->localize('Authentication'); ?>
 
@@ -45,6 +36,4 @@
 					</table>
 				</fieldset>
 			</form>
-		</div>
-	</body>
-</html>
+<? $this->displayView('components/footer.php'); ?>
